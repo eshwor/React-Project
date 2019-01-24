e8ca06629f9d25403abb8a7dff5bc235e1403c42
Ishwor <ishworkhatri26@yahoo.com>
2019-01-24 09:11:22 -0500

    updated files

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,6 +12,7 @@ const controls = [
 
 const buildControls = (props) => (
   <div className="BuildControls">
+    <p>Current Price: ${props.price.toFixed(2)}</p>
     {controls.map(ctrl => (
       <BuildControl
         key={ ctrl.label }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -66,7 +66,12 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients}/>
-        <BuildControls ingredientAdded={this.addIngredientHandler} ingredientRemoved={this.removeIngredientHandler} disabled={disabledInfo}/>
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+          />
       </Aux>
     );
   }
